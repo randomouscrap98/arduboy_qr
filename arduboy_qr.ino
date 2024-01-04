ff9160ffe9531c74fdf9b2e3e837be2d7a9fad06
Carlos Sanchez <randomouscrap98@aol.com>
2024-01-04 02:44:57 -0500

    First working version

--- arduboy_qr.ino
+++ arduboy_qr.ino
@@ -8,8 +8,9 @@ Arduboy2 arduboy;
 
 const uint8_t FRAMERATE = 60;
 
-const uint8_t MAXINPUT = 128;
-char input[MAXINPUT];
+const uint8_t MAXLINEWIDTH = 21;
+const uint8_t MAXINPUT = MAXLINEWIDTH * 3;
+char input[MAXINPUT + 1];
 
 const uint8_t MAXVERSION = 9;
 const qrcodegen_Ecc MINECC = qrcodegen_Ecc_MEDIUM;
@@ -19,16 +20,16 @@ const bool BOOSTECL = true;
 uint8_t qrbuffer[qrcodegen_BUFFER_LEN_FOR_VERSION(MAXVERSION)];
 uint8_t tempbuffer[qrcodegen_BUFFER_LEN_FOR_VERSION(MAXVERSION)];
 
-const uint8_t KEYBOARD_LINELENGTH = 20;
+const uint8_t KEYBOARD_LINELENGTH = 21;
 const uint8_t KEYBOARD_LINECOUNT = 5;
 const uint8_t KEYBOARD_STARTY = 3;
 const uint8_t KEYBOARD_STARTX = 0;
 
-const char KEYLINE1[] PROGMEM = "ABCDEFGHIJKLM[01234]";
-const char KEYLINE2[] PROGMEM = "NOPQRSTUVWXYZ(56789)";
-const char KEYLINE3[] PROGMEM = "abcdefghijklm{./@&=}";
-const char KEYLINE4[] PROGMEM = "nopqrstuvwxyz<?!,'\">";
-const char KEYLINE5[] PROGMEM = "#$%^*+-_=|\\:;~`     ";
+const char KEYLINE1[] PROGMEM = "ABCDEFGHIJKLM[01234]?";
+const char KEYLINE2[] PROGMEM = "NOPQRSTUVWXYZ(56789)!";
+const char KEYLINE3[] PROGMEM = "abcdefghijklm{./:@&}#";
+const char KEYLINE4[] PROGMEM = "nopqrstuvwxyz<+-=*%>$";
+const char KEYLINE5[] PROGMEM = "     |^_\\;,~`\"'      ";
 
 const char * KEYLINES[KEYBOARD_LINECOUNT];
 
@@ -92,6 +93,22 @@ void printKeyboard(uint8_t cx, uint8_t cy)
     setTextInvert(false);
 }
 
+void printInput()
+{
+    arduboy.fillRect(0, 0, 128, 24, BLACK);
+    arduboy.drawFastHLine(0, 22, 128, WHITE);
+    setTextCursor(0,0);
+    uint8_t offset = 0;
+    char line[MAXLINEWIDTH + 1];
+    while(offset < strlen(input) && offset < MAXINPUT)
+    {
+        memcpy(line, input + offset, MAXLINEWIDTH);
+        line[MAXLINEWIDTH] = 0;
+        arduboy.println(line);
+        offset += MAXLINEWIDTH;
+    }
+}
+
 char getKeyboardAt(uint8_t cx, uint8_t cy)
 {
     return pgm_read_byte(KEYLINES[cy] + cx);
@@ -118,6 +135,9 @@ void setStateAbout()
     arduboy.print(F(" * On: Down+Right+B\n"));
     arduboy.print(F(" * Off: Up+Right+B\n"));
     arduboy.print(F(" * Limit 100 chars\n"));
+
+    // This way, it's only reset if you go all the way to the title screen
+    memset(input, 0, MAXINPUT + 1);
 }
 
 void setStateEntry()
@@ -126,6 +146,7 @@ void setStateEntry()
     resetKeyboard();
     arduboy.clear();
     printKeyboard(kbx, kby);
+    printInput();
 }
 
 void setStateDisplay()
@@ -137,15 +158,26 @@ void setStateDisplay()
     generateQr(input);
 
     int size = qrcodegen_getSize(qrbuffer);
-    arduboy.fillRect(0, 0, size + 4, size + 4, WHITE);
+    int sizemod = floor(60.0 / size);
+    int realsize = sizemod * size;
+    int startx = (128 - realsize - 4) / 2;
+    int starty = (64 - realsize - 4) / 2;
+
+    arduboy.fillRect(startx, starty, realsize + 4, realsize + 4, WHITE);
     for (int y = 0; y < size; y++) {
         for (int x = 0; x < size; x++) {
-            arduboy.drawPixel(x + 2, y + 2, qrcodegen_getModule(qrbuffer, x, y) ? BLACK : WHITE);
+            arduboy.fillRect(startx + x * sizemod + 2, starty + y * sizemod + 2, sizemod, sizemod, qrcodegen_getModule(qrbuffer, x, y) ? BLACK : WHITE);
         }
     }
 }
 
 #define MENUWRAP(var, max, mov) { var = (var + mov + max) % max; }
+
+unsigned long lastInputStart = 0;
+unsigned long lastInputRepeat = 0;
+const uint8_t DIR_BUTTONS = UP_BUTTON | DOWN_BUTTON | LEFT_BUTTON | RIGHT_BUTTON;
+const uint16_t REPEATDELAY = 250;
+const uint16_t REPEATREPEAT = 50;
 
 void loop()
 {
@@ -159,20 +191,53 @@ void loop()
     }
     else if(state == GameState::Entry)
     {
-        if (arduboy.justPressed(UP_BUTTON))
-            MENUWRAP(kby, KEYBOARD_LINECOUNT, -1);
-        if (arduboy.justPressed(DOWN_BUTTON))
-            MENUWRAP(kby, KEYBOARD_LINECOUNT, 1);
-        if (arduboy.justPressed(LEFT_BUTTON))
-            MENUWRAP(kbx, KEYBOARD_LINELENGTH, -1);
-        if (arduboy.justPressed(RIGHT_BUTTON))
-            MENUWRAP(kbx, KEYBOARD_LINELENGTH, 1);
+        if (arduboy.anyPressed(DIR_BUTTONS))
+        {
+            unsigned long ms = millis();
 
-        if (arduboy.anyPressed(UP_BUTTON | DOWN_BUTTON | LEFT_BUTTON | RIGHT_BUTTON))
-            printKeyboard(kbx, kby);
+            if(ms - lastInputRepeat > REPEATREPEAT && ms - lastInputStart > REPEATDELAY)
+            {
+                if(!lastInputStart) lastInputStart = ms;
+                lastInputRepeat = ms;
+
+                if (arduboy.pressed(UP_BUTTON))
+                    MENUWRAP(kby, KEYBOARD_LINECOUNT, -1);
+                if (arduboy.pressed(DOWN_BUTTON))
+                    MENUWRAP(kby, KEYBOARD_LINECOUNT, 1);
+                if (arduboy.pressed(LEFT_BUTTON))
+                    MENUWRAP(kbx, KEYBOARD_LINELENGTH, -1);
+                if (arduboy.pressed(RIGHT_BUTTON))
+                    MENUWRAP(kbx, KEYBOARD_LINELENGTH, 1);
+
+                printKeyboard(kbx, kby);
+            }
+        }
+        else
+        {
+            //When they let go of the direction, don't delay their inputs anymore
+            lastInputRepeat = 0;
+            lastInputStart = 0;
+        }
+
+        if (arduboy.justPressed(A_BUTTON))
+        {
+            input[strlen(input)] = pgm_read_byte(KEYLINES[kby] + kbx);
+            printInput();
+        }
+        if (arduboy.justPressed(B_BUTTON) && !arduboy.anyPressed(DIR_BUTTONS))
+        {
+            input[max(0, strlen(input) - 1)] = 0;
+            printInput();
+        }
+
+        //Special combo to generate qr
+        if (arduboy.pressed(DOWN_BUTTON | RIGHT_BUTTON | B_BUTTON))
+            setStateDisplay();
     }
     else if(state == GameState::Display)
     {
+        if (arduboy.pressed(UP_BUTTON | RIGHT_BUTTON | B_BUTTON))
+            setStateEntry();
     }
 
     arduboy.display();
