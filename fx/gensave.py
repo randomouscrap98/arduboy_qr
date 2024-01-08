e71d85bc4e01c45e32b87dea4b7e8e4336e9dca0
Carlos Sanchez <randomouscrap98@aol.com>
2024-01-08 00:30:55 -0500

    working on testing ardens

--- fx/gensave.py
+++ fx/gensave.py
@@ -1,3 +1,7 @@
+import os
+
+SLOTSIZE = 4
+
 # Add your special strings here
 DEBUGDATA = [
     # "https://link1",
@@ -16,9 +20,13 @@ DEBUGDATA = [
 
 data = bytearray([255] * 4096)
 
+# Make it run in this directory
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
 # Need every 128th byte to be 0 to make strings "just work"
 di = 0
-for i in range(0, 4096, 128):
+for i in range(0, 4096, SLOTSIZE):
     if di < len(DEBUGDATA) and DEBUGDATA[di]:
         data[i:i+len(DEBUGDATA[di])] = DEBUGDATA[di].encode()
         data[i+len(DEBUGDATA[di])] = 0
